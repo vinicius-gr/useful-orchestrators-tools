692c33bc0ea10d6ba17f2d18948ffebb63d7bd13
Vinicius Gonzaga Rocha <viniciusgr@algartech.com>
2018-10-22 20:37:57 -0300

    added async to delay endpoint

--- api/controllers/utilsController.js
+++ api/controllers/utilsController.js
@@ -1,9 +1,21 @@
-exports.delay = (req, res, next) => {
-    const delayTime = req.body.delayTime;
+exports.delay = async (req, res, next) => {
+    try {
+        const delayTime = req.body.delayTime;
 
-    setTimeout(() => {
+        await delay(delayTime);
+
         res.status(200).json({
             message: `Everything went ok`
         });
-    }, delayTime);
+    } catch (error) {
+        res.status(500).json({
+            message: `Ops, somenthig went wrong`
+        });
+    }
+}
+
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
